4432d977394d2d6eea95553d3103e429e4b7e343
Brendan <brendan@cusackprep.com>
2021-09-28 18:37:02 -0400

    What is happening?

--- mongo_test.js
+++ mongo_test.js
@@ -8,7 +8,7 @@ MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
     console.log('Connected!');
 
     // database Name
-    const dbName ='Cluster0';
+    const dbName ='Cluster';
     const db = client.db(dbName);
 
     // new user
